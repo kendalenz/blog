4161abf1543597aa1ff708f9db13ce5ae6dc692c
Kendal Enz <kendal.enz@gmail.com>
2024-09-07 16:27:13 -0400

    added bio

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Kendal's Blog</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, I'm Kendal Enz. I'm a fullstack software engineer with a background in communications, journalism and creative writing. I enjoy building websites and writing about harnessing the power of AI to assist with coding and writing tasks. I hold an MA in Writing with a Concentration in Fiction from Johns Hopkins University, a BA in Communications with a Concentration in Journalism from Hood College and a Certificate of Software Engineering from Fullstack Academy. I'm based in Brooklyn, NY, and when I'm not working, I enjoy going on long runs through the city.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
