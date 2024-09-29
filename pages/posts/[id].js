0425c109bf721f862869e43dce679cd45e85a283
Kendal Enz <kendal.enz@gmail.com>
2024-09-29 18:07:52 -0400

    blog content displaying

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,13 +2,15 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+  
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -19,13 +21,15 @@ export async function getStaticPaths() {
 }
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
